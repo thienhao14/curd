2c711cc4765d507b6304bca009f3935e1dd3b64e
buithienhao-nlu <63627271+thienhao14@users.noreply.github.com>
2023-03-21 11:15:57 +0700

    Commit 1

--- src/main/java/com/example/demo/dao/CategoryDAO.java
+++ src/main/java/com/example/demo/dao/CategoryDAO.java
@@ -1,12 +1,11 @@
-package com.example.demo.service;
+package com.example.demo.dao;
 
 import java.util.List;
 
 import com.example.demo.model.Category;
 
-
-public interface CategoryService {
-	List<Category> get();
+public interface CategoryDAO {
+	List <Category> get();
 	Category get(int id);
 	void save(Category category);
 	void delete(int id);
---
+++ src/main/java/com/example/demo/model/Category.java
@@ -0,0 +1,75 @@
+package com.example.demo.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private Integer id;
+	@Column 
+	private String name;
+	@Column 
+	private String code;
+	@Column 
+	private String description;
+	@Column 
+	private Integer activeFlag;
+	@Column 
+	private Date createDate;
+	@Column 
+	private Date updateDate;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getActiveFlag() {
+		return activeFlag;
+	}
+	public void setActiveFlag(Integer activeFlag) {
+		this.activeFlag = activeFlag;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+	
+}
